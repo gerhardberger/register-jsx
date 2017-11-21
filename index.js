316eb7a7e1f80ad1b3324d5000a81e6b75a085c2
Heilig Benedek <benecene@gmail.com>
2017-11-21 16:23:17 +0100

    add ignore functionality using .jsxignore file

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
+const path = require('path')
+const fs = require('fs')
+
 const jsx = require('jsx-transform')
 const mime = require('mime-types')
+const findRoot = require('find-root')
+const minimatch = require('minimatch')
 
 const types = [
   'application/javascript',
   'text/jsx'
 ]
 
+let ignores = { }
+
 module.exports = (options) => {
   types.forEach(t => {
     const ext = mime.extension(t)
 
     require.extensions[`.${ext}`] = (module, filename) => {
-      const code = jsx.fromFile(filename, options)
-      module._compile(code, filename)
+      const isDependency = filename.indexOf('node_modules') >= 0
+
+      if (isDependency) {
+        module._compile(fs.readFileSync(filename, 'utf8'), filename)
+      } else {
+        try {
+          const root = findRoot(filename)
+          if (!ignores[root]) {
+            try {
+              const jsxignore = path.join(root, '.jsxignore')
+              ignores[root] = fs.readFileSync(jsxignore, 'utf8')
+                .split('\n')
+                .map(str => str.trim())
+                .filter(str => str.length > 0)
+            } catch (e) {
+              ignores[root] = []
+            }
+          }
+
+          const relative = path.relative(root, filename)
+          const isIgnored = !!ignores[root].find(ignore =>
+            minimatch(relative, ignore, { matchBase: true }))
+
+          if (isIgnored) {
+            module._compile(fs.readFileSync(filename, 'utf8'), filename)
+          } else {
+            module._compile(jsx.fromFile(filename, options), filename)
+          }
+        } catch (e) {
+          module._compile(jsx.fromFile(filename, options), filename)
+        }
+      }
     }
   })
 
